afbf105c15251c47b0d17c217ea640d64db232c7
Iffatusy Syaharani <vangelic@users.noreply.github.com>
2020-12-21 15:59:36 +0700

    ask

--- index/expert/all_question.php
+++ index/expert/all_question.php
@@ -1,2 +1,12 @@
 <?php
-echo "<h1> aku</h1>"; ?>
+        $dbc = new PDO('mysql:host=localhost;dbname=forum', 'root', '');
+        
+        $statement = $dbc->prepare("SELECT a.fullname, b.isi, c.judul FROM `user` a, `question` b, `topik` c WHERE a.id=b.id_user AND b.id_topic=c.id_topik");
+        $statement->execute();
+
+        foreach ($statement as $row) {
+            echo "<h1>{$row['fullname']}</h1>";
+            echo "<p>topik : {$row['judul']}</p>";
+            echo "<p>{$row['isi']}</p>";
+        }
+?>
