95788faad03f6fb2cd4d9a5b36ba73539c26e841
Iffatusy Syaharani <vangelic@users.noreply.github.com>
2020-12-22 17:25:31 +0700

    expert

--- index/expert/all_question.php
+++ index/expert/all_question.php
@@ -1,15 +1,15 @@
 <ul>
 	<?php 
 	include 'adminPermission.inc'; 
-	$statement = $db->prepare("SELECT a.fullname, b.isi, b.id_question, c.judul FROM `user` a, `question` b, `topik` c WHERE a.id=b.id_user AND b.id_topic=c.id_topik");
+	$statement = $db->prepare("SELECT a.fullname, b.pertanyaan, b.id_question, b.tanggal_dibuat_question, c.judul FROM `tb_user` a, `tb_question` b, `tb_topik` c WHERE a.id_user=b.id_user AND b.id_topik=c.id_topik");
     $statement->execute();
 	
 	foreach ($statement as $row) { ?>
 	<a href="?page=reply_quest&id=<?=$row['id_question']; ?>" class="question">
 		<li>
 			<h1><?php echo $row['fullname']?></h1>
-			<div class="isi"><?php echo $row['judul'] ?></div>
-			<div class="tanggal"><?php echo $row['isi'] ?></div>
+			<div class="isi"><?php echo $row['judul']." ".$row['tanggal_dibuat_question'] ?></div>
+			<div class="tanggal"><?php echo $row['pertanyaan'] ?></div>
 		</li>
 	</a>
 	<?php } ?>
--- index/expert/reply_quest.php
+++ index/expert/reply_quest.php
@@ -1,12 +1,46 @@
 <?php
-    $statement = $db->prepare('SELECT a.fullname, b.isi, b.id_question, c.judul FROM `user` a, `question` b, `topik` c WHERE b.id_topic = c.id_topik AND a.id=b.id_user AND b.id_question=:id');
+    include 'adminPermission.inc'; 
+    $statement = $db->prepare('SELECT a.fullname, b.pertanyaan, b.id_question, b.tanggal_dibuat_question, c.judul FROM `tb_user` a, `tb_question` b, `tb_topik` c WHERE b.id_topik = c.id_topik AND a.id_user=b.id_user AND b.id_question=:id');
     $statement->bindValue(':id', $_GET['id']);
     $statement->execute();
     $data = $statement->fetchAll();
 
     foreach ($data as $row) {
         echo "<h1>{$row['fullname']}</h1>";
-        echo "<p>{$row['judul']}</p>";
-        echo "<p>{$row['isi']}</p>";
+        echo "<p>{$row['judul']}"." "."{$row['tanggal_dibuat_question']}</p>";
+        echo "<p>{$row['pertanyaan']}</p>";
     }
+
+    $query = $db->prepare('SELECT c.fullname, a.tanggal_jawaban, a.jawaban FROM `tb_answer` a, `tb_question` b, `tb_user` c WHERE a.id_question=b.id_question AND a.id_user=c.id_user');
+	$query->execute();
+	$data = $query->fetchAll();
+	
+	foreach ($data as $question) {
+        echo "<h4>{$question['fullname']}</h4>";
+        echo "<p>{$question['tanggal_jawaban']}</p>";
+        echo "<p>{$question['jawaban']}</p>";
+    }
+
+    if (isset($_POST['submit'])) {
+		require '../config/validate.inc';
+		validateWajib($errors, $_POST, 'isi');
+		if($errors){
+			include 'formreply.php';
+		} else {
+			$state = $db->prepare("insert into tb_answer values (null, :id_user, :id_question, :reply ,:tgl)");
+			$state->bindValue(':id_question', $_GET['id']);
+			$state->bindValue(':id_user', $_SESSION['idUser']);
+			$state->bindValue(':reply', $_POST['isi']);
+			$state->bindValue(':tgl', date("Y-m-d H:i:s"));
+			$state->execute();
+			if ($state)
+			{
+				echo "<script>alert('Balasan Berhasil Diajukan');location.href='?page=reply_quest';</script>";
+			} else {
+				echo "<script>alert('Balasan Gagal Diajukan');location.href='?page=reply_quest';</script>";
+			}	
+		}
+	} else {
+        include 'formreply.php';
+    } 
 ?>
